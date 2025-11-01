a5832d56eb41db880761ae682f00c48a495a53a0
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-01 15:40:40 +0100

    Creato Movie model

---
+++ MovieRatingAPI/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MovieRatingAPI.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? Author { get; set; }
+        public int Year { get; set; }
+    }
+}
