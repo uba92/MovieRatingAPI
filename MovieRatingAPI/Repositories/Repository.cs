6d38af297b5488c474f1a00dd1ea358f82979860
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-02 12:21:27 +0100

    Implementazione Repository

---
+++ MovieRatingAPI/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRatingAPI.Interfaces;
+using MovieRatingAPI.Models;
+
+namespace MovieRatingAPI.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MovieContext _context;
+
+        public Repository(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(long id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }   
+
+        public async Task DeleteAsync(long id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
