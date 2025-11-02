7c6700cf3e5c8ad70b4d1e608c4beb1f08d9ee87
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-02 12:00:18 +0100

    Correzione GetByIdAsync

--- MovieRatingAPI/Interfaces/IRepository.cs
+++ MovieRatingAPI/Interfaces/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T?> GetAsyncById(long id);
+        Task<T?> GetByIdAsync(long id);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(long id);
