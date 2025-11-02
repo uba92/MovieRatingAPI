a52e392c7920257a18e20dba75191dd7ad713342
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-02 17:58:13 +0100

    implementazione IMovieService

---
+++ MovieRatingAPI/Interfaces/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using MovieRatingAPI.DTOs;
+
+namespace MovieRatingAPI.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieDTO>> GetAllAsync();
+        Task<MovieDTO?> GetByIdAsync(long id);
+        Task<MovieDTO> CreateAsync(MovieDTO movieDTO);
+        Task<MovieDTO?> UpdateAsync(long id, MovieDTO movieDTO);
+        Task<bool> DeleteAsync(long id);
+
+        Task<IEnumerable<MovieDTO>> GetTopRatedAsync();
+    }
+}
