f7cfc05f37a2d35be187c41424a6ef723c1cbd52
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-04 22:16:23 +0100

    Implementazione MovieController

--- MovieRatingAPI/Controllers/MoviesController.cs
+++ MovieRatingAPI/Controllers/MoviesController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieRatingAPI.DTOs;
+using MovieRatingAPI.Interfaces;
 using MovieRatingAPI.Models;
+using MovieRatingAPI.Services;
 
 namespace MovieRatingAPI.Controllers
 {
@@ -14,112 +17,75 @@ namespace MovieRatingAPI.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-        private readonly MovieContext _context;
+        private readonly IMovieService _movieService;
 
-        public MoviesController(MovieContext context)
+        public MoviesController(IMovieService movieService)
         {
-            _context = context;
+            _movieService = movieService;
         }
 
         // GET: api/Movies
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MovieReadDTO>>> GetMovies()
         {
-            return await _context.Movies.Select(x => MovieToDTO(x)).ToListAsync();
+            var movies = await _movieService.GetAllAsync();
+            return Ok(movies);
         }
 
-        // GET: api/Movies/5
+        //GET api/movies/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<MovieReadDTO>> GetMovie(long id)
         {
-            var movie = await _context.Movies.FindAsync(id);
+            var movie = await _movieService.GetByIdAsync(id);
 
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return MovieToDTO(movie);
-        }
-
-        // PUT: api/Movies/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovie(long id, MovieReadDTO movieDto)
-        {
-            if (id != movieDto.Id)
-            {
-                return BadRequest();
-            }
-
-            var movie = await _context.Movies.FindAsync(id);
             if(movie == null)
             {
                 return NotFound();
             }
 
-            movie.Title = movieDto.Title;
-            movie.Rating = movieDto.Rating;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException) when (!MovieExists(id))
-            {
-                return NotFound();
-            }
-
-            return NoContent();
+            return Ok(movie);
         }
 
-        // POST: api/Movies
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //POST: api/movies
         [HttpPost]
-        public async Task<ActionResult<MovieReadDTO>> PostMovie(MovieReadDTO movieDto)
+        public async Task<ActionResult<MovieReadDTO>> CreateMovie(MovieCreateDTO movie)
         {
-
-            var movie = new Movie
-            {
-                Title = movieDto.Title,
-                Rating = movieDto.Rating
-            };
-
-            _context.Movies.Add(movie);
-            await _context.SaveChangesAsync();
-
-            //return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
-            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, MovieToDTO(movie));
+            var createdMovie = await _movieService.CreateAsync(movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = createdMovie.Id }, createdMovie);
         }
 
-        // DELETE: api/Movies/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMovie(long id)
+        //PUT: api/movies/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult<MovieReadDTO>> UpdateMovie(long id, MovieUpdateDTO movie)
         {
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie == null)
+            var updatedMovie = await _movieService.UpdateAsync(id, movie);
+            if(updatedMovie == null)
             {
                 return NotFound();
             }
 
-            _context.Movies.Remove(movie);
-            await _context.SaveChangesAsync();
+            return Ok(updatedMovie);
+        }
+
+        //DELETE: api/movies/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMovie(long id)
+        {
+            var result = await _movieService.DeleteAsync(id);
+            if(!result)
+            {
+                return NotFound();
+            }
 
             return NoContent();
         }
 
-        // Converts a Movie to a MovieDTO
-        private static MovieReadDTO MovieToDTO(Movie movie) =>
-            new MovieReadDTO
-            {
-                Id = movie.Id,
-                Title = movie.Title,
-                Rating = movie.Rating
-            };
-
-        private bool MovieExists(long id)
+        //GET: api/movies/toprated
+        [HttpGet("toprated")]
+        public async Task<ActionResult<IEnumerable<MovieReadDTO>>> GetTopRated()
         {
-            return _context.Movies.Any(e => e.Id == id);
+            var movies = await _movieService.GetTopRatedAsync();
+            return Ok(movies);
         }
     }
 }
