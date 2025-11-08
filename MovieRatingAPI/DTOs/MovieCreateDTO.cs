dab5db208c7101889087eed2e06dcd4dff63b604
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-08 15:27:51 +0100

    add validation in MovieCreateDTO

--- MovieRatingAPI/DTOs/MovieCreateDTO.cs
+++ MovieRatingAPI/DTOs/MovieCreateDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace MovieRatingAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRatingAPI.DTOs
 {
     public class MovieCreateDTO
     {
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; } = string.Empty;
+        [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10.")]
         public int Rating { get; set; }
     }
 }
