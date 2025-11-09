f26e0501c6dd66be371beed467b7b0234fe72928
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-09 17:19:29 +0100

    Creazione IReviewService

--- MovieRatingAPI/Interfaces/IReviewService.cs
+++ MovieRatingAPI/Interfaces/IReviewService.cs
@@ -1,6 +1,12 @@
-﻿namespace MovieRatingAPI.Interfaces
+﻿using MovieRatingAPI.DTOs;
+
+namespace MovieRatingAPI.Interfaces
 {
     public interface IReviewService
     {
+        Task<IEnumerable<ReviewReadDTO>> GetReviewsByMovieIdAsync(long movieId);
+        Task<ReviewReadDTO> CreateReviewAsync(ReviewCreateDTO reviewDTO);
+        Task<ReviewReadDTO> UpdateReviewAsync(long movieid, ReviewUpdateDTO reviewUpdateDTO);
+        Task<bool> DeleteReviewAsync(long reviewId);
     }
 }
