f3289cb55fef03be24500ece82a1374e6e4bfd3c
Umberto Tramontano <u.tramontano92@libero.it>
2025-11-16 17:22:07 +0100

    endpoint in AuthController per la registrazione

---
+++ MovieRatingAPI/Controllers/Security/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRatingAPI.DTOs.Auth;
+using MovieRatingAPI.Interfaces.Security;
+
+namespace MovieRatingAPI.Controllers.Security
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var response = await _authService.RegisterAsync(request);
+            return Ok(response);
+        }
+    }
+}
