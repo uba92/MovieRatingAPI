9ce8ec7dbe82c418197dfa1537685bb8cf63369b
Umberto Tramontano <u.tramontano92@libero.it>
2025-12-06 17:14:40 +0100

    inizializzazione progetto di test

---
+++ MovieRatingAPI.Tests/Controllers/MovieControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace MovieRatingAPI.Tests.Controllers
+{
+    public class MovieControllerTests
+    {
+        [Fact]
+        public async Task Create_ShouldReturnCreatedAtAction_WhenMovieIsCreated()
+        {
+
+        }
+    }
+}
